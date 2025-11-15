0c030a4967809640dd97a72bed80bfa11c35e59b
Bati <no-reply@batijs.dev>
2025-11-15 15:03:13 +0000

    scaffold Vike app with Bati

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import tailwindcss from "@tailwindcss/vite";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
+import react from "@vitejs/plugin-react";
+import vike from "vike/plugin";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    vike(),
+    react(),
+    sentryVitePlugin({
+      sourcemaps: {
+        disable: false,
+      },
+    }),
+    tailwindcss(),
+  ],
+
+  build: {
+    sourcemap: true,
+  },
+
+  resolve: {
+    alias: {
+      "@": new URL("./", import.meta.url).pathname,
+    },
+  },
+});
